b72e95a56e242948942ae02980d18d40e96b2c30
TRMRai <davinkjohag@outlook.com>
2025-05-12 20:39:04 +0800

    fix: asan

--- packages/core_extensions/default_extension_go/tests/tester_test.go
+++ packages/core_extensions/default_extension_go/tests/tester_test.go
@@ -18,3 +18,15 @@ func TestMyExtensionTester(t *testing.T) {
 	tester.SetTestModeSingle("default_extension_go", "{}")
 	tester.Run()
 }
+
+// func TestMyExtensionTester2(t *testing.T) {
+// 	myTester := &MyExtensionTester{}
+
+// 	tester, err := ten.NewExtensionTester(myTester)
+// 	if err != nil {
+// 		t.FailNow()
+// 	}
+
+// 	tester.SetTestModeSingle("default_extension_go", "{}")
+// 	tester.Run()
+// }
--- tests/ten_runtime/integration/go/standalone_test_go/test_case.py
+++ tests/ten_runtime/integration/go/standalone_test_go/test_case.py
@@ -69,47 +69,29 @@ def test_standalone_test_go():
     if return_code != 0:
         assert False, "Failed to install package."
 
-
-    if sys.platform == "win32":
-        print("test_standalone_test_go doesn't support win32")
-        assert False
-    elif sys.platform == "darwin":
-        # client depends on some libraries in the TEN app.
-        my_env["DYLD_LIBRARY_PATH"] = os.path.join(
-            base_path,
-            "standalone_test_go_app/ten_packages/system/ten_runtime/lib",
-        )
-    else:
-        # client depends on some libraries in the TEN app.
-        my_env["LD_LIBRARY_PATH"] = os.path.join(
-            base_path,
-            "standalone_test_go_app/ten_packages/system/ten_runtime/lib",
-        )
-
-        build_config_args = build_config.parse_build_config(
-            os.path.join(root_dir, "tgn_args.txt"),
-        )
-
-        if (
-            build_config_args.enable_sanitizer
-            and not build_config_args.is_clang
-        ):
-            libasan_path = os.path.join(
-                base_path,
-                (
-                    "standalone_test_go_app/ten_packages/system/"
-                    "ten_runtime/lib/libasan.so"
-                ),
-            )
-            if os.path.exists(libasan_path):
-                print("Using AddressSanitizer library.")
-                my_env["LD_PRELOAD"] = libasan_path
-
     # Step 3:
     #
     # Run the test.
-    tester_process = subprocess.Popen(
+    test_cmd = [
         "tests/bin/start",
+    ]
+
+    build_config_args = build_config.parse_build_config(
+        os.path.join(root_dir, "tgn_args.txt"),
+    )
+
+    if build_config_args.enable_sanitizer:
+        test_cmd.append("-asan")
+
+    if build_config_args.is_clang:
+        my_env["CC"] = "clang"
+        my_env["CXX"] = "clang++"
+    else:
+        my_env["CC"] = "gcc"
+        my_env["CXX"] = "g++"
+
+    tester_process = subprocess.Popen(
+        test_cmd,
         stdout=stdout,
         stderr=subprocess.STDOUT,
         env=my_env,
