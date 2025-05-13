d287428c38b029fbeaac0078113b380bacd969e1
TRMRai <davinkjohag@outlook.com>
2025-05-13 10:51:52 +0800

    fix: support multi test cases in go standalone testing

--- packages/core_extensions/default_extension_go/tests/tester.go
+++ packages/core_extensions/default_extension_go/tests/tester.go
@@ -14,7 +14,19 @@ func (tester *MyExtensionTester) OnStart(tenEnvTester ten.TenEnvTester) {
 
 	fmt.Println("xxxxx OnStart")
 
-	tenEnvTester.StopTest()
+	cmdTest, _ := ten.NewCmd("test")
+	tenEnvTester.SendCmd(cmdTest, func(tet ten.TenEnvTester, cr ten.CmdResult, err error) {
+		if err != nil {
+			panic(err)
+		}
+
+		statusCode, _ := cr.GetStatusCode()
+		if statusCode != ten.StatusCodeOk {
+			panic(statusCode)
+		}
+
+		tenEnvTester.StopTest()
+	})
 }
 
 func (tester *MyExtensionTester) OnStop(tenEnvTester ten.TenEnvTester) {
--- packages/core_extensions/default_extension_go/tests/tester_test.go
+++ packages/core_extensions/default_extension_go/tests/tester_test.go
@@ -1,11 +1,55 @@
 package tests
 
 import (
-	"testing"
+	"fmt"
+	"os"
 	ten "ten_framework/ten_runtime"
+	"testing"
 
 	_ "default_extension_go"
 )
+
+var globalApp ten.App
+
+type fakeApp struct {
+	ten.DefaultApp
+
+	initDoneChan chan bool
+}
+
+func (p *fakeApp) OnInit(tenEnv ten.TenEnv) {
+	tenEnv.OnInitDone()
+	p.initDoneChan <- true
+}
+
+func setup() {
+	fakeApp := &fakeApp{
+		initDoneChan: make(chan bool),
+	}
+
+	var err error
+
+	globalApp, err = ten.NewApp(fakeApp)
+	if err != nil {
+		fmt.Println("Failed to create global app.")
+	}
+
+	globalApp.Run(true)
+
+	<-fakeApp.initDoneChan
+}
+
+func teardown() {
+	globalApp.Close()
+	globalApp.Wait()
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
 
 func TestMyExtensionTester(t *testing.T) {
 	myTester := &MyExtensionTester{}
@@ -19,14 +63,14 @@ func TestMyExtensionTester(t *testing.T) {
 	tester.Run()
 }
 
-// func TestMyExtensionTester2(t *testing.T) {
-// 	myTester := &MyExtensionTester{}
+func TestMyExtensionTester2(t *testing.T) {
+	myTester := &MyExtensionTester{}
 
-// 	tester, err := ten.NewExtensionTester(myTester)
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
+	tester, err := ten.NewExtensionTester(myTester)
+	if err != nil {
+		t.FailNow()
+	}
 
-// 	tester.SetTestModeSingle("default_extension_go", "{}")
-// 	tester.Run()
-// }
+	tester.SetTestModeSingle("default_extension_go", "{}")
+	tester.Run()
+}
